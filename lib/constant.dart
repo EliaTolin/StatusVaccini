db4888b1c6a181dd7a794d77ef50222d1af4fc54
Elia Tolin <eliatolin@gmail.com>
2021-03-20 19:03:22 +0100

    Fix uri

--- lib/constant.dart
+++ lib/constant.dart
@@ -14,14 +14,15 @@ abstract class SVConst {
   //COLOR CONSTANT
   static const mainColor = Colors.cyan;
   static const textColor = Colors.white;
-  static const backColor = Colors.white;
-
+  static Color backColor = Colors.grey.shade200;
+  static const iconColor = mainColor;
+  static const cardColor = Colors.white;
   //RADIUS ELEMENT
   static const double radiusComponent = 40;
 
   //SIZE ELEMENT
   static const double kHeighBarRatio = 0.15;
-  static const double kSizeIcons = 30;
+  static const double kSizeIcons = 50;
 }
 
 abstract class URLConst {
@@ -29,35 +30,43 @@ abstract class URLConst {
   //https://github.com/italia/covid19-opendata-vaccini/tree/master/dati
 
   //anagrafica-vaccini-summary-latest: totali delle somministrazioni per fasce d'età.
-  static const String anagraficaVacciniSummaryLatest =
-      'https://raw.githubusercontent.com/italia/covid19-opendata-vaccini/master/dati/anagrafica-vaccini-summary-latest.json';
+  static Uri anagraficaVacciniSummaryLatest = new Uri(
+      path:
+          'https://raw.githubusercontent.com/italia/covid19-opendata-vaccini/master/dati/anagrafica-vaccini-summary-latest.json');
 
   //consegne-vaccini-latest: dati sul totale delle consegne giornaliere dei vaccini suddivise per regioni.
-  static const String consegneVacciniLatest =
-      'https://raw.githubusercontent.com/italia/covid19-opendata-vaccini/master/dati/consegne-vaccini-latest.json';
+  static Uri consegneVacciniLatest = new Uri(
+      path:
+          'https://raw.githubusercontent.com/italia/covid19-opendata-vaccini/master/dati/consegne-vaccini-latest.json');
 
   //punti-somministrazione-latest: punti di somministrazione per ciascuna Regione e Provincia Autonoma.
-  static const String puntiSommistrazioneLatest =
-      'https://raw.githubusercontent.com/italia/covid19-opendata-vaccini/master/dati/punti-somministrazione-latest.json';
-  static const String puntiSommistrazioneTipologia =
-      'https://raw.githubusercontent.com/italia/covid19-opendata-vaccini/master/dati/punti-somministrazione-tipologia.json';
+  static Uri puntiSommistrazioneLatest = new Uri(
+      path:
+          'https://raw.githubusercontent.com/italia/covid19-opendata-vaccini/master/dati/punti-somministrazione-latest.json');
+  static Uri puntiSommistrazioneTipologia = new Uri(
+      path:
+          'https://raw.githubusercontent.com/italia/covid19-opendata-vaccini/master/dati/punti-somministrazione-tipologia.json');
 
   //somministrazioni-vaccini-latest: dati sulle somministrazioni giornaliere dei vaccini suddivisi per regioni,
   //fasce d'età e categorie di appartenenza dei soggetti vaccinati.
-  static const String somministrazioneVacciniLatest =
-      'https://raw.githubusercontent.com/italia/covid19-opendata-vaccini/master/dati/somministrazioni-vaccini-latest.json';
+  static Uri somministrazioneVacciniLatest = new Uri(
+      path:
+          'https://raw.githubusercontent.com/italia/covid19-opendata-vaccini/master/dati/somministrazioni-vaccini-latest.json');
 
   //somministrazioni-vaccini-summary-latest: dati sul totale delle somministrazioni giornaliere per regioni
   //e categorie di appartenenza dei soggetti vaccinati.
-  static const String sommistrazioneVacciniSummaryLatest =
-      'https://raw.githubusercontent.com/italia/covid19-opendata-vaccini/master/dati/somministrazioni-vaccini-summary-latest.json';
+  static Uri sommistrazioneVacciniSummaryLatest = new Uri(
+      path:
+          'https://raw.githubusercontent.com/italia/covid19-opendata-vaccini/master/dati/somministrazioni-vaccini-summary-latest.json');
 
   //vaccini-summary-latest: dati sul totale delle consegne e somministrazioni avvenute sino ad oggi, includendo la percentuale di
   //dosi somministrate (sul totale delle dosi consegnate) suddivise per regioni.
-  static const String vacciniSummaryLatest =
-      'https://raw.githubusercontent.com/italia/covid19-opendata-vaccini/master/dati/vaccini-summary-latest.json';
+  static Uri vacciniSummaryLatest = new Uri(
+      path:
+          'https://raw.githubusercontent.com/italia/covid19-opendata-vaccini/master/dati/vaccini-summary-latest.json');
 
   //last-update-dataset: data e ora di ultimo aggiornamento del dataset.
-  static const String lastUpdateDataSet =
-      'https://raw.githubusercontent.com/italia/covid19-opendata-vaccini/master/dati/last-update-dataset.json';
+  static Uri lastUpdateDataSet = new Uri(
+      path:
+          'https://raw.githubusercontent.com/italia/covid19-opendata-vaccini/master/dati/last-update-dataset.json');
 }
