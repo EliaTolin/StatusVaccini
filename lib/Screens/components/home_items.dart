4eccdf35357f3b6ada94e7d42b788c66de471abd
Elia Tolin <eliatolin@gmail.com>
2021-03-22 23:51:20 +0100

    Add new items in the homepage

--- lib/Screens/components/home_items.dart
+++ lib/Screens/components/home_items.dart
@@ -1,6 +1,8 @@
 import 'package:StatusVaccini/Models/opendata.dart';
-import 'package:StatusVaccini/Screens/components/graph_card.dart';
+import 'package:StatusVaccini/Screens/components/graph_linear_card.dart';
 import 'package:flutter/material.dart';
+
+import 'graph_pie_card.dart';
 
 class HomeItems {
   Widget card;
@@ -9,12 +11,30 @@ class HomeItems {
 
   static List<HomeItems> get items => [
         HomeItems(
-          card: GraphCard(
+          card: GraphPieCard(
             typeinfo: "Dosi",
+            labelText: "Dosi consegnate",
+            iconpath: "assets/order.svg",
+            funTextInformation: () => OpenData.getDosiTotali(),
+            funGetData: () => OpenData.graphDeliveryForDay(),
+          ),
+        ),
+        HomeItems(
+          card: GraphLinearCard(
+            typeinfo: "sommistrazioni",
             labelText: "Vaccini sommistrati",
             iconpath: "assets/virus.svg",
             funTextInformation: () => OpenData.getSomministrazioniTotali(),
             funGetData: () => OpenData.graphVacciniForDay(),
+          ),
+        ),
+        HomeItems(
+          card: GraphLinearCard(
+            typeinfo: "Dosi",
+            labelText: "Dosi consegnate",
+            iconpath: "assets/order.svg",
+            funTextInformation: () => OpenData.getDosiTotali(),
+            funGetData: () => OpenData.graphDeliveryForDay(),
           ),
         ),
       ];
