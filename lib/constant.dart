b6856af0efe0206c001e3fea704b4c1ffd6474f8
Elia Tolin <eliatolin@gmail.com>
2021-03-27 23:19:46 +0100

    Add new constant

--- lib/constant.dart
+++ lib/constant.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 
 //USE THIS CLASS FOR ONLY CONSTANT.
 
-abstract class SVConst {
+class SVConst {
   //TITLE APPBAR
   static const String titleAppBar = "Status Vaccini";
 
@@ -30,6 +30,55 @@ abstract class SVConst {
     Colors.amber,
     Colors.grey,
     Colors.cyan,
+  ];
+
+  static const List<Color> circularItemsColors = [
+    Color(0xFFF3722C),
+    Color(0xFFF49CBB),
+    Color.fromARGB(255, 172, 25, 100),
+    Color(0xFFF9C74F),
+    Color(0xFF90BE6D),
+    Color(0xFFE9AFA3),
+    Colors.cyan,
+    Color(0xFFF8961E),
+    Color(0xFF577590),
+    Color(0xFFBC2C1A),
+    Color(0xFF8980F5),
+    Colors.amber,
+    Color(0xFF417B5A),
+    Color(0xFF2EC0F9),
+    Color(0xFFDD2D4A),
+    Color(0xFFE9D2C0),
+    Color(0xFF880D1E),
+    Color(0xFFF26A8D),
+    Color(0xFF67AAF9),
+    Color(0xFFCBEEF3),
+    Color(0xFF43AA8B),
+    Color(0xFF2AB7CA),
+    Color.fromRGBO(255, 172, 25, 100),
+  ];
+
+  static const List<Color> circularFixItemsColors = [
+    Color(0xFFFF0054),
+    Colors.amber,
+    Color(0xFFF3722C),
+    Color(0xFFE9AFA3),
+    Color(0xFFF8961E),
+    Color(0xFFF9C74F),
+    Color(0xFF90BE6D),
+    Color(0xFF43AA8B),
+    Color(0xFF577590),
+    Color(0xFF67AAF9),
+    Color(0xFF2EC0F9),
+    Color(0xFFCBEEF3),
+    Color(0xFF8980F5),
+    Color(0xFFF49CBB),
+    Color(0xFFF26A8D),
+    Color(0xFFDD2D4A),
+    Color(0xFF880D1E),
+    Color(0xFFBC2C1A),
+    Color(0xFF2AB7CA),
+    Color(0xFF417B5A),
   ];
 
   static const List<Color> linearColors = [
