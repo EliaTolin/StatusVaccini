e73c90f740174552f41dd5948e54a907e9787469
Elia Tolin <eliatolin@gmail.com>
2021-04-07 21:03:01 +0200

    refactoring folder and add new items

--- lib/Screens/components/pageitems/home_items.dart
+++ lib/Screens/components/pageitems/home_items.dart
@@ -1,13 +1,13 @@
 import 'package:statusvaccini/models/opendata.dart';
-import 'package:statusvaccini/screens/components/graph_bar_card.dart';
-import 'package:statusvaccini/screens/components/graph_linear_card.dart';
-import 'package:statusvaccini/screens/components/graph_multiple_linear_card.dart';
-import 'package:statusvaccini/screens/components/regioni_card_view.dart';
-import 'package:statusvaccini/screens/components/graph_linear_ultime_consegne.dart';
-import 'package:statusvaccini/screens/components/graph_linear_ultime_sommistrazioni.dart';
+import 'package:statusvaccini/screens/components/graphs/graph_bar_card.dart';
+import 'package:statusvaccini/screens/components/graphs/graph_linear_card.dart';
+import 'package:statusvaccini/screens/components/graphs/graph_multiple_linear_card.dart';
+import 'package:statusvaccini/screens/components/cards/regioni_card_view.dart';
+import 'package:statusvaccini/screens/components/graphs/graph_linear_ultime_consegne.dart';
+import 'package:statusvaccini/screens/components/graphs/graph_linear_ultime_sommistrazioni.dart';
 import 'package:flutter/material.dart';
 
-import 'graph_pie_card.dart';
+import '../graphs/graph_pie_card.dart';
 
 class HomeItems {
   Widget card;
--- lib/Screens/views/recap_view.dart
+++ lib/Screens/views/recap_view.dart
@@ -1,6 +1,7 @@
 import 'package:statusvaccini/constants/constant.dart';
 import 'package:flutter/material.dart';
 import 'package:statusvaccini/screens/components/body_components.dart';
+import 'package:statusvaccini/screens/components/pageitems/recap_items.dart';
 
 class RecapScreen extends StatefulWidget {
   @override
@@ -12,9 +13,23 @@ class _RecapScreenState extends State<RecapScreen> {
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
     return Scaffold(
+      extendBody: true,
       appBar: TopBar(size, title: SVConst.titleAppBar),
-      body: Center(
-        child: Text("Hello, in Recap Screen!"),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(12),
+          child: Column(
+            children: <Widget>[
+              for (final recapItems in RecapItems.items) recapItems.card,
+              // TO DO
+              // FIX HEIGHT BODY.
+              // WITHOUT SIZEDBOX THE ELEMENT GOING UNDER BOTTOMBAR AT THE END
+              SizedBox(
+                height: 100,
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
