67c8a73458fd3bfb5d0c1103c1eaf227813df192
Elia Tolin <eliatolin@gmail.com>
2021-04-11 14:59:24 +0200

    Add new items

--- lib/Screens/components/pageitems/recap_items.dart
+++ lib/Screens/components/pageitems/recap_items.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:statusvaccini/screens/components/cards/consegne_card.dart';
 import 'package:statusvaccini/screens/components/cards/sommistrazioni_card.dart';
 
 class RecapItems {
@@ -9,8 +10,15 @@ class RecapItems {
   static List<RecapItems> get items => [
         RecapItems(
           card: SommistrazioniCard(
-            iconpath: "assets/icons/order.svg",
+            iconpath: "assets/icons/syringe.svg",
             firstLabel: "Sommistrazioni",
+            secondLabel: "Variazioni nei giorni",
+          ),
+        ),
+        RecapItems(
+          card: ConsegneCard(
+            iconpath: "assets/icons/fast_delivery.svg",
+            firstLabel: "Consegne dosi",
             secondLabel: "Variazioni nei giorni",
           ),
         ),
